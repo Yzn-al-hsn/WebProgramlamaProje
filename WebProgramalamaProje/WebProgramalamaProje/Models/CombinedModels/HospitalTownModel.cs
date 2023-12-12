2b2a33eb08e8cdc6c0f58f8cb4b13802f36f00a0
MHD-OMAR-SHAMIEH <103579045+MHD-OMAR-SHAMIEH@users.noreply.github.com>
2023-12-12 23:57:45 +0300

    achieving CRUD operations on Hospital model

---
+++ WebProgramalamaProje/WebProgramalamaProje/Models/CombinedModels/HospitalTownModel.cs
@@ -0,0 +1,8 @@
+﻿namespace WebProgramalamaProje.Models.CombinedModels
+{
+    public class HospitalTownModel
+    {
+        public HospitalModel Hospital { get; set; }
+        public List<TownModel> Towns     { get; set; }
+    }
+}
