97f56f56255f11a19fe8e1cb4f73d462d03546ad
yazan alhasan <110408280+yazan-al-hasan@users.noreply.github.com>
2024-01-01 19:09:21 +0300

    appointment take for ublic

--- WebProgramalamaProje/WebProgramalamaProje/Controllers/HomeController.cs
+++ WebProgramalamaProje/WebProgramalamaProje/Controllers/HomeController.cs
@@ -1,8 +1,12 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Localization;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System.Diagnostics;
+using WebProgramalamaProje.Data;
 using WebProgramalamaProje.Models;
+using WebProgramalamaProje.Models.CombinedModels;
 using WebProgramalamaProje.Service;
 
 namespace WebProgramalamaProje.Controllers
@@ -11,11 +15,15 @@ namespace WebProgramalamaProje.Controllers
     {
         private readonly LanguageService _localization;
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public HomeController(ILogger<HomeController> logger, LanguageService localization)
+        public HomeController(ILogger<HomeController> logger, LanguageService localization, ApplicationDbContext db, UserManager<ApplicationUser> userManager)
         {
             _logger = logger;
             _localization = localization;
+            _db = db;
+            _userManager = userManager;
         }
 
         public IActionResult ChangeLanguage(string culture)
@@ -48,6 +56,78 @@ namespace WebProgramalamaProje.Controllers
         public IActionResult Privacy()
         {
             return View();
+        }
+
+        public async Task<IActionResult> TakeAppointment()
+        {
+            var appointment = new AppointmentDoctorUserModel();
+
+            appointment.Doctors = (from doc in _db.Doctors
+                                   join cli in _db.Clinics on doc.ClinicId equals cli.Id
+                                   select new DoctorModel
+                                   {
+                                       Id = doc.Id,
+                                       Name = doc.Name + " - " + cli.Name,
+
+                                   }).ToList();
+
+            return View(appointment);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> TakeAppointment(AppointmentDoctorUserModel model)
+        {
+            var user = await _userManager.GetUserAsync(User);
+            model.Appointment.ClientId = user.Id;
+
+            var query = from x in _db.Appointments
+                        where ((x.StartDateTime <= model.Appointment.StartDateTime && x.EndDateTime >= model.Appointment.StartDateTime)
+                        || (x.StartDateTime <= model.Appointment.EndDateTime && x.EndDateTime >= model.Appointment.EndDateTime )
+                        || (x.StartDateTime>=model.Appointment.StartDateTime && x.EndDateTime<=model.Appointment.EndDateTime))
+                        && x.StartDateTime.DayOfYear == model.Appointment.StartDateTime.DayOfYear
+                        && x.EndDateTime.DayOfYear == model.Appointment.EndDateTime.DayOfYear
+                        && x.DoctorId == model.Appointment.DoctorId
+                        select x;
+
+            var startLessThanEnd = model.Appointment.StartDateTime <= model.Appointment.EndDateTime;
+
+            if (query.Any() ||!startLessThanEnd)
+            {
+                model.Doctors = (from doc in _db.Doctors
+                                 join cli in _db.Clinics on doc.ClinicId equals cli.Id
+                                 select new DoctorModel
+                                 {
+                                     Id = doc.Id,
+                                     Name = doc.Name + " - " + cli.Name,
+
+                                 }).ToList();
+
+                ViewBag.ErrorBool = true;
+                ViewBag.ErrorMsg = _localization.GetKey("AppointmentError");
+
+                return View(model);
+            }
+            
+
+            await _db.Appointments.AddAsync(model.Appointment);
+            await _db.SaveChangesAsync();
+
+            return RedirectToAction("AppointmentList");
+        }
+
+        public async Task<IActionResult> AppointmentList()
+        {
+            var user = await _userManager.GetUserAsync(User);
+
+            HttpClient client = new HttpClient();
+
+            var response = await client.GetAsync("https://localhost:44318/api/AppointmentApi");
+            var jsonResponse = await response.Content.ReadAsStringAsync();
+            var Appointments = JsonConvert.DeserializeObject<List<AppointmentExtraModel>>(jsonResponse);
+
+            Appointments = Appointments.Where(a => a.ClientId == user.Id).ToList();
+
+            return View(Appointments);
         }
 
 
