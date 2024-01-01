058605b887c49c3bf2d3847bb9821ff154da6283
MHD-OMAR-SHAMIEH <103579045+MHD-OMAR-SHAMIEH@users.noreply.github.com>
2024-01-01 21:14:28 +0300

    .

--- WebProgramalamaProje/WebProgramalamaProje/Models/Enums/Days.cs
+++ WebProgramalamaProje/WebProgramalamaProje/Models/Enums/Days.cs
@@ -2,12 +2,12 @@
 {
     public enum Days
     {
-        Monday = 0,
-        Tuesday = 1,
-        Wednesday = 2,
-        Thursday = 3,
-        Friday = 4, 
-        Saturday = 5,
-        Sunday = 6,
+        Sunday = 0,
+        Monday = 1,
+        Tuesday = 2,
+        Wednesday = 3,
+        Thursday = 4,
+        Friday = 5, 
+        Saturday = 6,
     }
 }
